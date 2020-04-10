2717ae8d7d0f9e979dd7759831ee3c58fd6af897
ZalGe <tomekfilip7@gmail.com>
2020-04-10 22:52:56 +0200

    writing an encrypted text to the file, reading decrypting text from the file

--- Database/Database.java
+++ Database/Database.java
@@ -1,12 +1,14 @@
 package Database;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Database {
     Scanner sc = new Scanner(System.in);
-    private String email;
     private String password;
-    private int DefaultPasswordLenght = 10;
+
     /**
      * Enter new elements to the database
      */
@@ -14,12 +16,49 @@ public class Database {
     {
         /* Constructor to receive email */
         System.out.println("Enter your E-mail:");
-        email = sc.nextLine();
+        String email = sc.nextLine();
         //System.out.println(email);
 
         /* Call a method asking for password */
-        this.password = setPass(DefaultPasswordLenght);
+        int defaultPasswordLenght = 10;
+        this.password = setPass(defaultPasswordLenght);
         System.out.println(password);
+
+        /* Writing to the file */
+        try
+        {
+            FileWriter writer = new FileWriter("Database.txt", true);
+            writer.write(Encrypting.encryptIt((email + " - " + password), "192748749364") + "\n");
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Read elements from the database
+     */
+    public void ReadDatabase()
+    {
+        String encrypt;
+        String decrypt;
+        try {
+            FileReader reader = new FileReader("Database.txt");
+            Scanner scan = new Scanner(reader);
+            while(scan.hasNextLine())
+            {
+                encrypt = scan.nextLine();
+                decrypt = Encrypting.decryptIt((encrypt), "192748749364");
+                System.out.println(decrypt);
+            }
+            reader.close();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
     /**
@@ -47,6 +86,4 @@ public class Database {
         }
         else{return "Something went wrong. Process was terminated. Try to restart the program.";}
     }
-
-    
 }
